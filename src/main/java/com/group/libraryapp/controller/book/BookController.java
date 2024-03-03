0e50c97e22420e48d98f3e9163e3341d630ae5ee
ghktndyd <hwasuyong99@naver.com>
2024-03-03 20:54:29 +0900

    book 구현

--- src/main/java/com/group/libraryapp/controller/book/BookController.java
+++ src/main/java/com/group/libraryapp/controller/book/BookController.java
@@ -1,16 +1,36 @@
 package com.group.libraryapp.controller.book;
 
+import com.group.libraryapp.dto.book.request.BookCreateRequest;
+import com.group.libraryapp.dto.book.request.BookLoanRequest;
+import com.group.libraryapp.dto.book.request.BookReturnRequest;
 import com.group.libraryapp.service.book.BookService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class BookController {
 
-    private final BookService bookService;
+  private final BookService bookService;
 
-    public void saveBook() {
+  public BookController(BookService bookService) {
+    this.bookService = bookService;
+  }
 
-    }
+  @PostMapping("/book")
+  public void saveBook(@RequestBody BookCreateRequest request) {
+    bookService.saveBook(request);
+  }
+
+  @PostMapping("/book/loan")
+  public void loanBook(@RequestBody BookLoanRequest request) {
+    bookService.loanBook(request);
+  }
+
+  @PutMapping("/book/return")
+  public void returnBook(@RequestBody BookReturnRequest request) {
+    bookService.returnBook(request);
+  }
+
 }
---
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -0,0 +1,28 @@
+package com.group.libraryapp.domain.book;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Getter
+    @Column(nullable = false)
+    private String name;
+
+    protected Book() {
+
+    }
+
+    public Book(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다", name));
+        }
+        this.name = name;
+    }
+
+}
