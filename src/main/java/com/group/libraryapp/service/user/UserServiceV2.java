6b9f3b578870d8141fed2155d6986029a6bcb3c1
ghktndyd <hwasuyong99@naver.com>
2024-03-03 20:46:40 +0900

    transactional

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -7,6 +7,7 @@ import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,27 +18,29 @@ public class UserServiceV2 {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         User savedUser = userRepository.save(new User(request.getName(), request.getAge()));
         savedUser.getId();
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(user -> new UserResponse(user.getId(), user.getName(), user.getAge()))
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
         // 유저가 없다면 IllegalArgumentException 예외 발생
 
         user.updateName(request.getName());
-
-        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // select * from user where name = ?;
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
